540ae7b866bc37c725388099e078364401519978
codeGiche <51116440+codeGiche@users.noreply.github.com>
2019-08-14 15:18:36 +0300

    updated the .get function

--- downloading_from_net.py
+++ downloading_from_net.py
@@ -1,3 +1,6 @@
 Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 22:22:05) [MSC v.1916 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+>>> import urllib.requests
+url="https://www.google.com/search?q=image&tbm=isch&source=iu&ictx=1&fir=ejCcUwDgWnW99M%253A%252CQooZsUX3wOLfLM%252C_&vet=1&usg=AI4_-kQ94gAz_EzFfz74_V89_qzrbYkHJA&sa=X&ved=2ahUKEwiN476IooLkAhWt3eAKHSTnA2wQ9QEwAHoECAUQBg#imgrc=ejCcUwDgWnW99M:"
+requests.get(url,"video")
+
